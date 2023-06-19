a9afd7496de0f720fd1f090fb93705985331e198
kasi <danielkaser@mailbox.org>
2023-06-19 12:16:26 +0200

    install swr and mongoose

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+import useSWR from "swr";
 // import Image from "next/image";
 // import { Inter } from "next/font/google";
 
 // const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  // const url = "../../_ressources/demoStocks.json";
+  // const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  // const { data } = useSWR(url, fetcher);
+  // console.log(data);
+
+  // render data
+  // return <div>Hello {data.name}!</div>; // Hello Luke Skywalker!
+
   return (
     <>
       <div className="text-red-500 bg-slate-600 hover:bg-slate-800">test</div>
