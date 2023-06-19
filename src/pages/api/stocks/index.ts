49bd1cac8815dfd84c14884f2cb057f0776b279f
kasi <danielkaser@mailbox.org>
2023-06-19 12:27:41 +0200

    create handler in api/stocks/index.ts

---
+++ src/pages/api/stocks/index.ts
@@ -0,0 +1,13 @@
+import dbConnect from "../../../db/connect";
+import Joke from "../../../db/models/Joke";
+
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const jokes = await Joke.find();
+    return response.status(200).json(jokes);
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
+  }
+}
