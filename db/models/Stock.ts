89db259abc0b3c2c4803d904978da109bb0637eb
kasi <danielkaser@mailbox.org>
2023-06-19 14:56:37 +0200

    refactor Stock-Model and api/stocks/index.ts; delete DemoStock-Model

--- db/models/Stock.ts
+++ db/models/Stock.ts
@@ -3,9 +3,12 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const stockSchema = new Schema({
+  ticker: { type: String, required: true },
+  // ticker: String,
   name: String,
+  exchange: String,
+  assetType: String,
   //name: { type: String, required: true },
-  type: String,
 });
 
 // check whether the model with this name has already been compiled and if yes, take the already compiled model
--- src/pages/api/stocks/index.ts
+++ src/pages/api/stocks/index.ts
@@ -1,6 +1,5 @@
 import dbConnect from "../../../../db/connect";
-import DemoStock from "../../../../db/models/DemoStock";
-// import Stock from "../../../../db/models/Stock";
+import Stock from "../../../../db/models/Stock";
 
 // TS: NextApiRequest and NextApiResponse types from next,
 // TS: provide type checking for the request and response objects.
@@ -12,12 +11,14 @@ export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
+  //
   await dbConnect();
+  // console.log("test");
 
   if (request.method === "GET") {
-    const DemoStocks = await DemoStock.find();
-    console.log("DemoStocks from DB:", DemoStocks);
-    return response.status(200).json(DemoStocks);
+    const stocks = await Stock.find();
+    console.log("DemoStocks from DB:", stocks);
+    return response.status(200).json(stocks);
   } else {
     return response.status(405).json({ message: "HTTP Method not allowed" });
   }
