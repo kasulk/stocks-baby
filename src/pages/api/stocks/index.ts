6b8c24ec9ce8d9501f524afc8c19aac5b301d2ae
kasi <danielkaser@mailbox.org>
2023-06-19 12:52:41 +0200

    add TS to stocks route handler

--- src/pages/api/stocks/index.ts
+++ src/pages/api/stocks/index.ts
@@ -1,13 +1,23 @@
-import dbConnect from "../../../db/connect";
-import Joke from "../../../db/models/Joke";
+import dbConnect from "../../../../db/connect";
+import DemoStock from "../../../../db/models/DemoStock";
+// import Stock from "../../../../db/models/Stock";
 
-export default async function handler(request, response) {
+// TS: NextApiRequest and NextApiResponse types from next,
+// TS: provide type checking for the request and response objects.
+import { NextApiRequest, NextApiResponse } from "next";
+
+// TS: Type annotation to the handler function, indicating that
+// TS: it expects a NextApiRequest and a NextApiResponse object
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
   await dbConnect();
 
   if (request.method === "GET") {
-    const jokes = await Joke.find();
-    return response.status(200).json(jokes);
+    const DemoStocks = await DemoStock.find();
+    return response.status(200).json(DemoStocks);
   } else {
-    return response.status(405).json({ message: "Method not allowed" });
+    return response.status(405).json({ message: "HTTP Method not allowed" });
   }
 }
