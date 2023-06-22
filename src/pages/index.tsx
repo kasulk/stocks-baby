25f95bf8eeb998db5fb2bd2cc2ff608c35219ad0
kasi <danielkaser@mailbox.org>
2023-06-22 14:16:32 +0200

    refactor sort logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,34 +35,44 @@ export default function Home() {
 
   // step: Make sort function dynamic, depending on chosen dropdown-value
   // step: e.g.: 'Symbol-ascending' => a.Symbol (take keyname and direction from dropdown-value)
-  // const sortedStocks = stocks.slice().sort((a: String, b: String) => {
-  if (sortParam.sortDirection === "ascending") {
-    // const sortedStocks: StockType[] = stocks
-    stocks
-      // .slice()
-      .sort((a: StockType, b: StockType) => {
-        // if ascending
-        if (a.Symbol < b.Symbol) {
-          return -1;
-        }
-        if (a.Symbol > b.Symbol) {
-          return 1;
-        }
-      });
-  } else if (sortParam.sortDirection === "descending") {
-    // const sortedStocks: StockType[] = stocks
-    stocks
-      // .slice()
-      .sort((a: StockType, b: StockType) => {
-        // if ascending
-        if (a.Symbol < b.Symbol) {
-          return 1;
-        }
-        if (a.Symbol > b.Symbol) {
-          return -1;
-        }
-      });
-  }
+
+  stocks.sort((a: StockType, b: StockType) => {
+    if (sortParam.sortDirection === "ascending") {
+      if (a.Symbol < b.Symbol) {
+        return -1;
+      }
+      if (a.Symbol > b.Symbol) {
+        return 1;
+      }
+    } else if (sortParam.sortDirection === "descending") {
+      if (a.Symbol < b.Symbol) {
+        return 1;
+      }
+      if (a.Symbol > b.Symbol) {
+        return -1;
+      }
+    }
+  });
+  // }
+  // if (sortParam.sortDirection === "ascending") {
+  //   stocks.sort((a: StockType, b: StockType) => {
+  //     if (a.Symbol < b.Symbol) {
+  //       return -1;
+  //     }
+  //     if (a.Symbol > b.Symbol) {
+  //       return 1;
+  //     }
+  //   });
+  // } else if (sortParam.sortDirection === "descending") {
+  //   stocks.sort((a: StockType, b: StockType) => {
+  //     if (a.Symbol < b.Symbol) {
+  //       return 1;
+  //     }
+  //     if (a.Symbol > b.Symbol) {
+  //       return -1;
+  //     }
+  //   });
+  // }
   // console.log("stocks:", stocks);
   // console.log("sortedStocks:", sortedStocks);
 
