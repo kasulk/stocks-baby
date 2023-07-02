1e8be3e727b2c527c41546288662f216f4dc77cd
kasi <kasulk@mailbox.org>
2023-07-02 19:27:39 +0200

    create IconMagnifiyingGlass comp for SearchForm

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -1,17 +1,19 @@
+import IconMaginifiyingGlass from "../IconMagnifiyingGlass";
+
 type Props = {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 export default function SearchForm({ onChange }: Props) {
   return (
-    <form method="POST" className="relative">
-      {/* <div className="absolute top-4 left-4 z-10">🔍</div> */}
-      <label htmlFor="search" className="absolute top-4 left-4 z-10">
-        🔍
-      </label>
+    <form
+      method="POST"
+      className="flex items-center transition hover:scale-x-105 bg-slate-200 hover:bg-slate-300 active:bg-slate-400 focus:outline-slate-300 shadow-md hover:shadow-lg active:shadow-none rounded-md py-2 px-4 m-2 md:mr-4 mr-0"
+    >
+      <IconMaginifiyingGlass className="fill-slate-500 mr-2" size={20} />
       <input
         id="search"
-        className="transition hover:scale-x-105 bg-slate-200 hover:bg-slate-300 active:bg-slate-400 focus:border-none focus:outline-slate-300 shadow-md hover:shadow-lg active:shadow-none rounded-md py-2 px-4 pl-12 m-2 md:mr-4 mr-0"
+        className="px-4 outline-none bg-slate-200"
         type="text"
         onChange={(event) => onChange(event)}
         placeholder="company name / symbol"
