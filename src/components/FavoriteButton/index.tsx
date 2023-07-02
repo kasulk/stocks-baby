4fb7e9256e1b95c00976d12a777dfb0878f5aa3f
kasi <kasulk@mailbox.org>
2023-07-02 18:36:29 +0200

    create Heart comp for the heart svg

--- src/components/FavoriteButton/index.tsx
+++ src/components/FavoriteButton/index.tsx
@@ -1,3 +1,5 @@
+import Heart from "../Heart";
+
 type Props = {
   _id: string;
   onToggleFavorite: (id: string, user: string) => void;
@@ -16,19 +18,28 @@ export default function FavoriteButton({
   const isFavorite = Favorites?.includes(currentUser);
 
   return (
-    <svg
+    <Heart
+      size={32}
+      onClick={() => onToggleFavorite(_id, currentUser)}
       className={`absolute top-4 right-4 cursor-pointer transition active:fill-slate-100 active:stroke-slate-100 active:transition-none hover:scale-125  ${
         isFavorite
           ? `fill-rose-900 scale-110 hover:fill-slate-500`
           : `fill-transparent stroke-slate-300 stroke-2 scale-90 hover:fill-rose-900 hover:stroke-none`
       }`}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      height={size}
-      width={size}
-      onClick={() => onToggleFavorite(_id, currentUser)}
-    >
-      <path d="m12 21.35-1.45-1.32C5.4 15.36 2 12.27 2 8.5 2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53L12 21.35Z" />
-    </svg>
+    />
+    // <svg
+    //   className={`absolute top-4 right-4 cursor-pointer transition active:fill-slate-100 active:stroke-slate-100 active:transition-none hover:scale-125  ${
+    //     isFavorite
+    //       ? `fill-rose-900 scale-110 hover:fill-slate-500`
+    //       : `fill-transparent stroke-slate-300 stroke-2 scale-90 hover:fill-rose-900 hover:stroke-none`
+    //   }`}
+    //   xmlns="http://www.w3.org/2000/svg"
+    //   viewBox="0 0 24 24"
+    //   height={size}
+    //   width={size}
+    //   onClick={() => onToggleFavorite(_id, currentUser)}
+    // >
+    //   <path d="m12 21.35-1.45-1.32C5.4 15.36 2 12.27 2 8.5 2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53L12 21.35Z" />
+    // </svg>
   );
 }
--- src/components/ShowFavoriteStocksToggle/index.tsx
+++ src/components/ShowFavoriteStocksToggle/index.tsx
@@ -1,9 +1,11 @@
-import Heart from "../../../_ressources/heart.svg";
+// import Heart from "../../../_ressources/heart.svg";
+import Heart from "@/components/Heart";
 
 type Props = {
   isShowFavoriteStocks: boolean;
   setIsShowFavoriteStocks: React.Dispatch<React.SetStateAction<boolean>>;
 };
+// function onClick(): void {}
 
 export default function ShowFavoriteStocksToggle({
   isShowFavoriteStocks,
@@ -14,8 +16,15 @@ export default function ShowFavoriteStocksToggle({
       className="transition hover:scale-x-105 bg-slate-200 hover:bg-slate-300 active:bg-slate-400 shadow-md hover:shadow-lg active:shadow-none rounded-md py-2 px-4 m-2 md:mr-4 mr-0"
       onClick={() => setIsShowFavoriteStocks(!isShowFavoriteStocks)}
     >
-      {isShowFavoriteStocks ? "Show All" : "❤ Favorites "}
-      {/* {isShowFavoriteStocks ? "Show All" : <Heart />} */}
+      {/* {isShowFavoriteStocks ? "Show All" : "❤ Favorites "} */}
+      {isShowFavoriteStocks ? (
+        "Show All"
+      ) : (
+        <span className="flex items-center">
+          <Heart size={20} className="fill-red-500" />
+          Favorites
+        </span>
+      )}
     </button>
   );
 }
