39a22840e4933e004f58aa735de778be64887b31
kasi <danielkaser@mailbox.org>
2023-07-04 17:03:17 +0200

    style dark mode toggle

--- src/components/DarkmodeToggle/index.tsx
+++ src/components/DarkmodeToggle/index.tsx
@@ -4,6 +4,14 @@ type Props = {
 };
 
 export default function DarkmodeToggle({ onClick, theme }: Props) {
-  return <button onClick={onClick}>{theme === "dark" ? "☀" : "🌙"}</button>;
+  return (
+    <button
+      //  className="w-8 h-8 rounded-full border"
+      className="w-8 h-8 transition rounded-full hover:scale-x-105 bg-accent-1 hover:bg-accent-2 active:bg-accent-3 shadow-sm shadow-accent-1 hover:shadow-md hover:shadow-accent-1 active:shadow-none m-2 md:mr-4 mr-0"
+      onClick={onClick}
+    >
+      {theme === "dark" ? "☀" : "🌙"}
+    </button>
+  );
 }
 //
