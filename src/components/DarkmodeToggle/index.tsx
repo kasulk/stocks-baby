b0e2087581b7c29c5f1ce69c1e553b1d6a51769b
kasi <danielkaser@mailbox.org>
2023-08-04 15:26:28 +0200

    workaround hydration error in darkmodetoggle comp

--- src/components/DarkmodeToggle/index.tsx
+++ src/components/DarkmodeToggle/index.tsx
@@ -6,6 +6,9 @@ type Props = {
 export default function DarkmodeToggle({ onClick, theme }: Props) {
   return (
     <button
+      // workaround hydration error...
+      // https://nextjs.org/docs/messages/react-hydration-error
+      suppressHydrationWarning
       // className="w-10 h-10 transition rounded-full hover:scale-x-105 bg-accent-1 hover:bg-accent-2 active:bg-accent-3 shadow-sm shadow-accent-1 hover:shadow-md hover:shadow-accent-1 active:shadow-none m-2 md:mr-4 mr-0"
       className="w-10 h-10 transition rounded-full hover:scale-x-105 bg-accent-1 hover:bg-accent-2 active:bg-accent-3 shadow-sm shadow-accent-1 hover:shadow-md hover:shadow-accent-1 active:shadow-none ml-2"
       onClick={onClick}
