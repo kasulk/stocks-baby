b1b11e15c2fa777826242eeff4e27d5027d58092
kasi <danielkaser@mailbox.org>
2023-08-04 13:46:40 +0200

    refactor usePagination hook

--- src/utils/usePagination.tsx
+++ src/utils/usePagination.tsx
@@ -2,15 +2,14 @@ import useSWRInfinite, { SWRInfiniteKeyLoader } from "swr/infinite";
 
 // export default function usePagination<T>(url: string) {
 export default function usePagination(url: string) {
-  const PAGE_SIZE = 12;
+  const ITEMS_PER_PAGE = 12;
 
   const getKey: SWRInfiniteKeyLoader = (pageIndex, previousPageData) => {
     pageIndex++;
     console.log(pageIndex);
 
     if (previousPageData && !previousPageData.length) return null;
-    // return `/api/stocks?page=${pageIndex}&limit=${PAGE_SIZE}`;
-    return `${url}?page=${pageIndex}&limit=${PAGE_SIZE}`;
+    return `${url}?page=${pageIndex}&limit=${ITEMS_PER_PAGE}`;
   };
   const {
     data: stocks,
@@ -21,9 +20,6 @@ export default function usePagination(url: string) {
     mutate,
   } = useSWRInfinite(getKey);
 
-  // const flattenedStocks = stocks?.flat();
-  // const flattenedStocks = stocks?.flatMap((page) => page);
-  // console.log("flattenedStocks:", flattenedStocks);
   const paginatedStocks = stocks?.flat();
   console.log("paginatedStocks :", paginatedStocks);
 
@@ -33,7 +29,7 @@ export default function usePagination(url: string) {
 
   const isEmpty = stocks?.[0]?.length === 0;
   const isReachingEnd =
-    isEmpty || (stocks && stocks[stocks.length - 1]?.length < PAGE_SIZE);
+    isEmpty || (stocks && stocks[stocks.length - 1]?.length < ITEMS_PER_PAGE);
 
   return {
     paginatedStocks,
