4d5700ac3364b3abcfd2a9819e6fbbb3531488a5
kasi <danielkaser@mailbox.org>
2023-08-04 13:45:56 +0200

    refactor api/stocks

--- src/pages/api/stocks/index.ts
+++ src/pages/api/stocks/index.ts
@@ -16,9 +16,6 @@ export default async function handler(
   await dbConnect();
 
   if (request.method === "GET") {
-    //! check if paginierung works in the api route
-    //! e.g. http://localhost:3000/data?page=10&limit=5
-    // https://www.golinuxcloud.com/paginate-with-mongoose-in-node-js/
     const { page = 1, limit = 12 } = request.query as {
       page?: number;
       limit?: number;
@@ -27,13 +24,12 @@ export default async function handler(
     // request Overviews and combine with Quotes and Logourls based on the common field 'ticker'
     const stocks = await Overview.find()
       .sort({ ticker: 1 })
-      // .limit(pageSize)
       .limit(limit * 1)
       .skip((page - 1) * limit)
       .populate("quotesData")
-      .populate("logoData"); // note: icke test
+      .populate("logoData");
 
-    const count = await Overview.count();
+    // const count = await Overview.count();
 
     return response.status(200).json(stocks);
     // return response.status(200).json({
@@ -47,7 +43,9 @@ export default async function handler(
   // @patchrequest, step3
   if (request.method === "PATCH") {
     await toggleUserToStockFavorites(request, response);
-  } else {
+  }
+  //
+  else {
     return response.status(405).json({ message: "HTTP Method not allowed" });
   }
 }
@@ -58,15 +56,19 @@ async function toggleUserToStockFavorites(
 ) {
   const { id, Favorites } = request.body;
   try {
-    const demostockToUpdate = await Overview.findById(id);
-    if (demostockToUpdate.Favorites.includes(Favorites)) {
+    // const demostockToUpdate = await Overview.findById(id);
+    const stockToUpdate = await Overview.findById(id);
+    // if (demostockToUpdate.Favorites.includes(Favorites)) {
+    if (stockToUpdate.Favorites.includes(Favorites)) {
       await Overview.findOneAndUpdate({ _id: id }, { $pull: { Favorites } });
       // console.log(`User '${Favorites}' removed from Favorites array`);
-      response.status(200).json(demostockToUpdate);
+      // response.status(200).json(demostockToUpdate);
+      response.status(200).json(stockToUpdate);
     } else {
       await Overview.findOneAndUpdate({ _id: id }, { $push: { Favorites } });
       // console.log(`User '${Favorites}' added to Favorites array`);
-      response.status(200).json(demostockToUpdate);
+      // response.status(200).json(demostockToUpdate);
+      response.status(200).json(stockToUpdate);
     }
   } catch (error) {
     console.error("Error updating the Favorites Array...", error);
