4852e39243253fb2885153eaf179a062e7b4fa73
kasi <danielkaser@mailbox.org>
2023-08-17 17:49:47 +0200

    change size of darkmode toggle for smaller screens

--- src/components/DarkmodeToggle/index.tsx
+++ src/components/DarkmodeToggle/index.tsx
@@ -12,7 +12,7 @@ export default function DarkmodeToggle({ onClick, theme }: Props) {
       // https://nextjs.org/docs/messages/react-hydration-error
       suppressHydrationWarning
       // className="w-10 h-10 transition rounded-full hover:scale-x-105 bg-accent-1 hover:bg-accent-2 active:bg-accent-3 shadow-sm shadow-accent-1 hover:shadow-md hover:shadow-accent-1 active:shadow-none m-2 md:mr-4 mr-0"
-      className="w-10 h-10 transition rounded-full hover:scale-x-105 bg-accent-1 hover:bg-accent-2 active:bg-accent-3 shadow-sm shadow-accent-1 hover:shadow-md hover:shadow-accent-1 active:shadow-none ml-2"
+      className="text-xs sm:text-base w-6 sm:w-10 h-6 sm:h-10 transition rounded-full hover:scale-x-105 bg-accent-1 hover:bg-accent-2 active:bg-accent-3 shadow-sm shadow-accent-1 hover:shadow-md hover:shadow-accent-1 active:shadow-none ml-2"
       onClick={onClick}
     >
       {theme === "dark" ? "☀" : "🌙"}
