1c38e20acc2132c9c3cd161afa72f324a9b8d11f
kasi <danielkaser@mailbox.org>
2023-08-17 17:41:12 +0200

    change size of search input for smaller screens

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -10,15 +10,15 @@ export default function SearchForm({ onChange, searchQuery }: Props) {
   return (
     <form
       method="POST"
-      className="flex items-center transition hover:scale-x-105 bg-accent-1 hover:bg-accent-2 active:bg-accent-3 focus:outline-accent-2 shadow-sm shadow-accent-1 hover:shadow-md hover:shadow-accent-1 active:shadow-none rounded-md py-2 px-4 mx-2 lg:mx-4 mb-3"
+      className="flex items-center transition hover:scale-x-105 bg-accent-1 hover:bg-accent-2 active:bg-accent-3 focus:outline-accent-2 shadow-sm shadow-accent-1 hover:shadow-md hover:shadow-accent-1 active:shadow-none rounded-md py-1 px-2 sm:py-2 sm:px-4 mx-2 lg:mx-4 mb-3"
     >
       <IconMaginifiyingGlass
-        className="fill-customcontentcolor mr-2"
+        className="fill-customcontentcolor mr-2 h-3 sm:h-auto"
         size={20}
       />
       <input
         id="search"
-        className="px-4 outline-none bg-accent-1"
+        className="px-4 outline-none bg-accent-1 text-xs sm:text-base"
         // type="text"
         type="search"
         onChange={(event) => onChange(event)}
